d43bc46d41d7e8d3b25b44effd4b76e39a5a5d6a
Zuodong Yu <richzyu@gmail.com>
2014-10-03 19:31:08 +0800

    update

--- apps.js
+++ apps.js
@@ -1,40 +1,51 @@
 function update(e){
-	var value=document.querySelector("input[type=text]").value;
-	if (document.querySelector("input[type=checkbox]").checked){
+	var value=it.value;
+	if (value.search(/blob:/)>=0){
+		ic.checked=true;
+	}
+	if (ic.checked){
 		value="url("+value+")";
 	}else{
 		value="'"+value+"'";
 	}
 	document.styleSheets[0].addRule("#output::before","content: "+value+"!important;");
-	document.querySelector('#output').style.border="solid 1px rgb(131, 131, 131)";
-	document.querySelector('#output').style.background="";
-	document.querySelector('#output').style.paddingLeft="10px";
-	document.querySelector('#output').style.paddingRight="10px";
-	document.querySelector('#output').style.maxWidth="400px";
-	document.querySelector('#output').innerHTML = marked(document.querySelector("textarea.auto").value+"\n\n--------\n<p style='font-size: 0.7em'>本文由 archion.github.io/changweibo 在线生成<p>");
+	var output=document.querySelector('#output');
+	output.style.border="solid 1px rgb(131, 131, 131)";
+	output.style.background="";
+	output.style.paddingLeft="10px";
+	output.style.paddingRight="10px";
+	output.style.maxWidth="400px";
+	output.innerHTML = marked(document.querySelector("textarea.auto").value+"\n\n--------\n<p style='font-size: 0.7em'>本文由 archion.github.io/changweibo 在线生成<p>");
 	$('pre code').each(function(i, block) {
 		hljs.highlightBlock(block);
 	});
 	MathJax.Hub.Typeset();
 }
-function dropFile(el){
+function dropFile(el,handler){
 	el.addEventListener("dragover", function(e) {
 		e.preventDefault();
 	},false)
 	el.addEventListener("drop", function(e) {
 		e.preventDefault();
-		var target=e.target;
-		target.value=target.value.substring(0,target.selectionStart)+window.URL.createObjectURL(e.dataTransfer.files[0])+target.value.substring(target.selectionEnd);
+		handler(e);
 	},false)
 };
+function getURL(e){
+	var target=e.target;
+	if (target.tagName.toLowerCase()=="textarea"){
+		target.value=target.value.substring(0,target.selectionStart)+"\n![]("+window.URL.createObjectURL(e.dataTransfer.files[0])+")\n"+target.value.substring(target.selectionEnd);
+	}else{
+		target.value=target.value.substring(0,target.selectionStart)+window.URL.createObjectURL(e.dataTransfer.files[0])+target.value.substring(target.selectionEnd);
+	}
+}
 var ta=document.querySelector("textarea");
 var bt=document.querySelector("#gn");
 var it=document.querySelector("input[type=text]")
 var ic=document.querySelector("input[type=checkbox]")
 ta.addEventListener("keyup",update,false)
 ta.addEventListener("focus",update,false)
-dropFile(ta);
-dropFile(it);
+dropFile(ta,getURL);
+dropFile(it,getURL);
 it.addEventListener("keyup",update,false)
 it.addEventListener("focus",update,false)
 it.addEventListener("drop",update,false)
